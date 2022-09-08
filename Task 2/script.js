dda83162ed39e336da35e982fcb396e9add3b63a
TomasGrigas <tomas.grigas@icloud.com>
2022-09-08 19:59:58 +0300

    Task 2 Done.

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,13 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+let counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("btn__state").innerHTML = val;
+}
