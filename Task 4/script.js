26081bcb73152cfdaffc3c0f282daa0137e934c0
TomasGrigas <tomas.grigas@icloud.com>
2022-09-09 20:40:00 +0300

    Task 4 Done.

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,24 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
+
 const ENDPOINT = 'cars.json';
+const request  = fetch(ENDPOINT);
+const listEl = document.querySelector('ul');
+request
+.then((res)=> res.json())
+.then((data) => {
+    data.forEach(post => {
+        listEl.insertAdjacentHTML('beforeend',`<li>${post.brand} - ${post.models} </li>`);
+    });
+});
+
+
+
+
+
+
+
+
+
+
